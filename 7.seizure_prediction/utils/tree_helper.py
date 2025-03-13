ae125f020124871f4a19fae2d216c6626a1d3ec0
huang_wanhong <huangwanhong39@gmail.com>
2025-03-13 17:41:44 +0900

    Add input embeddings.

--- 7.seizure_prediction/utils/tree_helper.py
+++ 7.seizure_prediction/utils/tree_helper.py
@@ -15,14 +15,19 @@ def deserialize_tree(tree_str):
             return None
 
         # Extract the 6 values for the current node
-        value = (
+        value = [
             int(tokens[index]),     # std::get<0>
             int(tokens[index + 1]), # std::get<1>
             int(tokens[index + 2]), # std::get<2>
             int(tokens[index + 3]), # std::get<3>
             float(tokens[index + 4]), # std::get<4>
             int(tokens[index + 5])  # std::get<5>
-        )
+        ]
+        value[0] = 0 if value[0] == 65535 else value[0] + 1
+        value[1] = 0 if value[1] == 65535 else value[1] + 1
+        value[2] = 0 if value[2] == 65535 else value[2] + 1
+        value[5] = 0 if value[5] == 65535 else value[5] + 1
+
         index += 6
 
         # Recursively deserialize left and right children
