7dcbf792848d8383e1bc450b7fbe5343aa1747d7
WanhongHUANG <huangwanhong39@gmail.com>
2025-05-09 11:03:32 +0900

    Upload Unix scripts.

---
+++ 5.syntax_analysis/make_syntax_analysis_program.sh
@@ -0,0 +1,2 @@
+make -C ../lib/pcfg-cky-inside-outside/ clean
+make -C ../lib/pcfg-cky-inside-outside/ syntax_analysis -j
---
+++ 5.syntax_analysis/syntax_analysis.sh
@@ -0,0 +1,7 @@
+BINARY_PATH=../lib/pcfg-cky-inside-outside/bin/syntax_analysis
+$BINARY_PATH config_normal-test.yaml
+$BINARY_PATH config_normal-val.yaml
+$BINARY_PATH config_preepileptic.yaml
+$BINARY_PATH config_seizure.yaml
+$BINARY_PATH config_normal-train.yaml
+
