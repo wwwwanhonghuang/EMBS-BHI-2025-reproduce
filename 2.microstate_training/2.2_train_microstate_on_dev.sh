5fc7d9257c1808545bdd5a7f673d5ee024a37447
Ubuntu <ubuntu@localhost.localdomain>
2025-05-13 14:22:54 +0800

    Fix microstate training bash scripts.

--- 2.microstate_training/2.2_train_microstate_on_dev.sh
+++ 2.microstate_training/2.2_train_microstate_on_dev.sh
@@ -2,7 +2,7 @@
 
 FORCE_REPREPROCESSING=0
 CONFIGURATION_FILE=./configs/config-all-person-microstate-dev.json
-PREEPILEPTIC_LENGTH_SEC = 60
+PREEPILEPTIC_LENGTH_SEC=60
 
 FLAGS="--pre_epileptic_zone $PREEPILEPTIC_LENGTH_SEC --database_index_configuration $CONFIGURATION_FILE"
 
